39108cc7e286a30278b2f4e0a6ec678ef215e4da
vonbrank <vonbrank@outlook.com>
2023-09-16 01:44:50 +0800

    build: 更新打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import typescript from "@rollup/plugin-typescript";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+
+function resolve(str: string) {
+  return path.resolve(__dirname, str);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    outDir: "lib",
+    lib: {
+      entry: resolve("src/lib/index.ts"),
+      name: "MaterialUiToastWrapper",
+    },
+    rollupOptions: {
+      external: [
+        "react",
+        "react-dom",
+        "react-transition-group",
+        "uuid",
+        "@emotion/react",
+        "@emotion/styled",
+        "@mui/material",
+        "@mui/icons-material",
+      ],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+  plugins: [
+    react(),
+    libInjectCss(),
+    typescript({
+      target: "es5",
+      rootDir: resolve("src/lib"),
+      outDir: resolve("lib"),
+      declaration: true,
+      declarationDir: resolve("lib"),
+      exclude: resolve("node_modules/**"),
+      allowSyntheticDefaultImports: true,
+    }),
+  ],
+});
